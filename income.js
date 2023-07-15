5ad43d512ae6e9f158f5f0e1d27839af55bdc7d5
8940148677SIVARAM <117276140+8940148677SIVARAM@users.noreply.github.com>
2023-07-16 01:05:35 +0530

    Update income.js

--- income.js
+++ income.js
@@ -1,8 +1,11 @@
-
+siva2002ram(false);
 let form=document.querySelector("form");
 			form.addEventListener('submit',function(s){
 				s.preventDefault();
-				story=document.getElementById("story");
+				siva2002ram(true)
+		});
+function siva2002ram(first){
+	story=document.getElementById("story");
 				transactionname=document.getElementById("name");
 				transactionamount=document.getElementById("Amount");
 			   let  value=document.getElementById("name").value;
@@ -11,13 +14,13 @@ let form=document.querySelector("form");
 			   let  out=document.getElementById("expence");
 			   let  bal=document.getElementById("balance");
 			   let a=JSON.parse(localStorage.getItem("transaction")) || [];
-				if((name=='') || value==''){
+				if((name=='' || value=='') && first){
 				  alert("please Ender the value");
 				 }
-				  else if((!isNaN(value))){
+				  else if((!isNaN(value) && first)){
 					  alert("name of transaction is only for letters");
 				  }
-				  else if(isNaN(name)){
+				  else if(isNaN(name) && first){
 					alert("amount is only for numbers");
 				  }
 			  else{
@@ -25,13 +28,24 @@ let form=document.querySelector("form");
 				  name:value,
 				  amount:name
 				}
+				if(!(b.name==="")){
 				value="";
 				amount="";
 				a.push(b);
 				localStorage.setItem("transaction",JSON.stringify(a));
 				document.getElementById("name").value="";
 				document.getElementById("Amount").value="";
-				let h=JSON.parse(localStorage.getItem("transaction"));
+				ram();
+			  }
+			  else{
+				  ram();
+			  }
+			  let h=JSON.parse(localStorage.getItem("transaction"));
+			story.innerHTML=h.map((e,i)=>`<li class=${e.amount<0 ?"red":"normal"}><h6>transaction:</h6><h6>Amount:</h6><span>${e.name}</span><span>${e.amount}</span><button onclick="delt(${i})" type="submit"}>remove</button></li>`);
+
+}
+function ram(){
+	let h=JSON.parse(localStorage.getItem("transaction"));
 				let income=[];
 				let expence=[];
 				h.map((e)=>
@@ -47,6 +61,5 @@ let form=document.querySelector("form");
 				}
 				bal.innerHTML=parseInt(out.innerHTML)+parseInt(inc.innerHTML)});
 			  }
-			  let h=JSON.parse(localStorage.getItem("transaction"));
-			story.innerHTML=h.map((e,i)=>`<li class=${e.amount<0 ?"red":"normal"}>Name of the transaction:<span>${e.name}</span>;Amount of paying:<span>${e.amount}</span><button onclick="delt(${i})" type="submit"}>remove</button></li>`);
-		});
+}
+		
